5d313bb849f30882650356db7db4c94255df3e69
Angel04172002 <58862906+Angel04172002@users.noreply.github.com>
2022-11-01 01:50:25 +0200

    Create StartUp.cs

---
+++ Inheritance/Farm/StartUp.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace Farm
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+
+            dog.Eat();
+
+            dog.Bark();
+
+            Cat cat = new Cat();
+
+            cat.Eat();
+
+            cat.Meow();
+        }
+    }
+}
+
