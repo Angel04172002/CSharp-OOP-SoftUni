0f5ee8e5812b7ed14c0117b6e4bfcf350a6e1060
Angel04172002 <58862906+Angel04172002@users.noreply.github.com>
2022-11-01 12:14:03 +0200

    Create StartUp.cs

---
+++ Inheritance/Custom Random List/StartUp.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            RandomList list = new RandomList();
+            list.Add("1");
+            list.Add("2");
+            list.Add("3");
+            var element = list.RandomString();
+            Console.WriteLine(element);
+        }
+    }
+}
