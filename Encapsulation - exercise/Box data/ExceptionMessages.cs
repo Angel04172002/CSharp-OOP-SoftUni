f99b442286277283c761f9df8c0df0b815ad4d84
Angel04172002 <58862906+Angel04172002@users.noreply.github.com>
2022-11-06 16:54:23 +0200

    Create ExceptionMessages.cs

---
+++ Encapsulation - exercise/Box data/ExceptionMessages.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassBoxData
+{
+    public static class ExceptionMessages
+    {
+        public const string ZeroOrNegativeError = "{0} cannot be zero or negative.";
+    }
+}
