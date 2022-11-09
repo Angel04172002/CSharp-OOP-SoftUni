3154d00de4a3e36b0b497c2451d62623980eea91
Angel04172002 <58862906+Angel04172002@users.noreply.github.com>
2022-11-09 18:49:10 +0200

    Create Tesla.cs

---
+++ Abstraction and Interfaces/Cars/Tesla.cs
@@ -0,0 +1,30 @@
+
+namespace Cars
+{
+    public class Tesla : ICar, IElectricCar
+    {
+        public Tesla(string model, string color, int battery)
+        {
+            this.Model = model;
+            this.Color = color;
+            this.Battery = battery;
+        }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public int Battery { get; set; }
+
+        public string Start()
+        {
+            return "Engine start";
+        }
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Color} Tesla {this.Model} with {this.Battery} Batteries";
+        }
+    }
+}
