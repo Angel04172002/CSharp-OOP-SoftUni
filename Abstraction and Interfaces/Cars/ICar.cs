bdca38c2df5c80c53e80e0063e055054071f7be5
Angel04172002 <58862906+Angel04172002@users.noreply.github.com>
2022-11-09 18:49:50 +0200

    Create ICar.cs

---
+++ Abstraction and Interfaces/Cars/ICar.cs
@@ -0,0 +1,11 @@
+
+namespace Cars
+{
+    public interface ICar
+    {
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public string Start();
+        public string Stop();
+    }
+}
