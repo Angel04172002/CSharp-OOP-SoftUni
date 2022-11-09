2cd0d1bbc0c013f31752607ad5dde66c3aa80211
Angel04172002 <58862906+Angel04172002@users.noreply.github.com>
2022-11-09 18:49:28 +0200

    Create Seat.cs

---
+++ Abstraction and Interfaces/Cars/Seat.cs
@@ -0,0 +1,26 @@
+
+namespace Cars
+{
+    public class Seat : ICar
+    {
+        public Seat(string model, string color)
+        {
+            this.Model = model;
+            this.Color = color;
+        }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public string Start()
+        {
+            return "Engine start";
+        }
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+        public override string ToString()
+        {
+            return $"{this.Color} Seat {this.Model}";
+        }
+    }
+}
