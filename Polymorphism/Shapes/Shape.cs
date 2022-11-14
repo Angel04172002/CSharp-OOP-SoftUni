89cdc857f6e830552a35a4f44455a8697371b507
Angel04172002 <58862906+Angel04172002@users.noreply.github.com>
2022-11-14 15:40:30 +0200

    Create Shape.cs

---
+++ Polymorphism/Shapes/Shape.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public abstract class Shape
+    {
+        public abstract double CalculatePerimeter();
+        public abstract double CalculateArea();
+        public virtual string Draw()
+        {
+            return "";
+        }
+    }
+}
