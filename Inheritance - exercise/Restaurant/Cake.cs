432f87ffc46ee60e0081f374f66eb4925bd058b2
Angel04172002 <58862906+Angel04172002@users.noreply.github.com>
2023-04-07 23:20:45 +0300

    Add files via upload

---
+++ Inheritance - exercise/Restaurant/Cake.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Cake : Dessert
+    {
+        private const double Grams = 250;
+
+        private const double Calories = 1000;
+
+        private const decimal CakePrice = 5;
+
+        public Cake(string name)
+            :base(name, CakePrice, Grams, Calories)
+        {
+
+        }
+    }
+}
