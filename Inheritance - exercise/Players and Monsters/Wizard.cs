b6986ddce241448eed9dd9c8f6777192be4a3283
Angel04172002 <58862906+Angel04172002@users.noreply.github.com>
2023-04-07 22:52:59 +0300

    Create Wizard.cs

---
+++ Inheritance - exercise/Players and Monsters/Wizard.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlayersAndMonsters
+{
+    public class Wizard : Hero
+    {
+        public Wizard(string username, int level) 
+            : base(username, level)
+        {
+
+        }
+    }
+}
