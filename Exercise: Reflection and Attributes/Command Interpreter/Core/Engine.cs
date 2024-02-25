578bf5f7f557507347cd1297f14f9a4ca45917d2
Angel1980 <58862906+Angel04172002@users.noreply.github.com>
2024-02-25 13:34:47 +0200

    Add files via upload

---
+++ Exercise: Reflection and Attributes/Command Interpreter/Core/Engine.cs
@@ -0,0 +1,24 @@
+﻿namespace CommandPattern.Core
+{
+    using CommandPattern.Core.Contracts;
+    using System;
+    public class Engine : IEngine
+    {
+        private readonly ICommandInterpreter interpreter;
+
+        public Engine(ICommandInterpreter interpreter)
+        {
+            this.interpreter = interpreter; 
+        }
+
+        public void Run()
+        {
+            while(true)
+            {
+                string input = Console.ReadLine();
+                string result = interpreter.Read(input);
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
---
+++ Exercise: Reflection and Attributes/Command Interpreter/Utilities/CommandInterpreter.cs
@@ -0,0 +1,47 @@
+﻿namespace CommandPattern.Utilities
+{
+    using CommandPattern.Core.Contracts;
+    using System;
+    using System.Linq;
+    using System.Reflection;
+
+    public class CommandInterpreter : ICommandInterpreter
+    {
+        public string Read(string args)
+        {
+            string[] tokens = args.Split(' ');
+            string commandName = tokens[0];
+
+            string[] parameters = tokens
+                .Skip(1)
+                .ToArray();
+
+            var currentAssembly = Assembly.GetEntryAssembly();
+
+            var currentType = currentAssembly
+                .GetTypes()
+                .FirstOrDefault(t => t.Name == $"{commandName}Command");
+
+            if (currentType == null)
+            {
+                throw new InvalidOperationException("Invalid type");
+            }
+
+
+            var method = currentType
+                .GetMethods()
+                .FirstOrDefault(m => m.Name == "Execute");
+
+            if (method == null)
+            {
+                throw new InvalidOperationException("Invalid method");
+            }
+
+            var obj = Activator.CreateInstance(currentType);
+
+            var result = (string)method.Invoke(obj, new object[] { parameters });
+
+            return result;
+        }
+    }
+}
