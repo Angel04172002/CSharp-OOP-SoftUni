f825408bd187ec2a91ab9c809aeb2a3d9dce02c3
Angel1980 <58862906+Angel04172002@users.noreply.github.com>
2024-02-25 15:22:15 +0200

    Create StartUp.cs

---
+++ Reflection and Attributes/Mission Private Impossible/StartUp.cs
@@ -0,0 +1,13 @@
+namespace Stealer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Spy spy = new Spy();
+
+            string result = spy.RevealPrivateMethods("Hacker");
+            Console.WriteLine(result);
+        }
+    }
+}
