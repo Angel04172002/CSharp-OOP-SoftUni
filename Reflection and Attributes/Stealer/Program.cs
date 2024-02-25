fa490062b6452eeb73a81d55050c166a0bfdec1b
Angel1980 <58862906+Angel04172002@users.noreply.github.com>
2024-02-25 15:02:12 +0200

    Create Program.cs

---
+++ Reflection and Attributes/Stealer/Program.cs
@@ -0,0 +1,13 @@
+namespace Stealer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Spy spy = new Spy();
+
+            string result = spy.StealFieldInfo("Hacker", "username", "password");
+            Console.WriteLine(result);
+        }
+    }
+}
